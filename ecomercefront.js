1b598cfe1905ff58df19680e6d8b760f4f1e70b0
Margaço Marine <Marine@mbp-de-margaco.home>
2019-03-13 09:50:20 +0100

    First commit

--- ecomercefront.js
+++ ecomercefront.js
@@ -239,61 +239,61 @@ products.forEach(function (product) {
 })*/
 
 //LE TABLEAU JS EST PASSé DANS UNE FONCTION
-function afficherPlusieursProduits(tableauDeProduits){
+function afficherPlusieursProduits(tableauDeProduits) {
     tableauDeProduits.forEach(function (product) {
 
         //JE CREE DES DIV QUI AURONT POUR ID MON PRODUCT NAME
         var elementProduct = document.createElement('div');
         elementProduct.setAttribute('id', product.name);
         container.appendChild(elementProduct);
-    
+
         //JE CREE DES IMG AVEC URL DE MES PRODUITS
         var imageProduct = document.createElement('img');
         imageProduct.setAttribute('src', product.url);
         elementProduct.appendChild(imageProduct);
-    
+
         //JE CREE DES ZONES DE TEXTE QUI VONT CHERCHER LE PRIX ET LE NOM
         var priceAndNameProduct = document.createElement('p');
         var priceAndName = document.createTextNode(product.name + " " + product.price);
         elementProduct.appendChild(priceAndName);
-    
+
         //JE CREE UN BOUTON AVEC LA VALEUR PURCHASE
         var buy = document.createElement('input');
         var button = document.createTextNode('Purchase');
         buy.type = "button";
-        buy.style = 'width: 90px; height: 20px;';
+        buy.style = 'width: 70px; height: 20px;';
         buy.value = "PURCHASE";
         elementProduct.appendChild(buy);
-    
+
         //JE LUI MET UN EVENT LISTENER 
         buy.addEventListener("click", function () {
             console.log('jai acheté');
         });
-    
+
         //FAIRE FONCTIONNER MON LIKE
         var aLike = document.createElement('img');
         aLike.setAttribute('class', 'unlike');
         aLike.setAttribute('src', 'http://assets.stickpng.com/thumbs/585e4e6ccb11b227491c339e.png');
         elementProduct.appendChild(aLike);
-    
+
         //FUNCTION LIKE DISLIKE
         function displayLike(aLike, like) {
-    
+
             if (like == true) {
                 aLike.setAttribute('src', 'http://assets.stickpng.com/thumbs/585e4e6ccb11b227491c339e.png');
                 console.log(aLike);
             } else {
                 aLike.setAttribute('src', 'https://3.bp.blogspot.com/-Pok2zariw6w/WP39o3cKhmI/AAAAAAAACo4/Tn7Ju2RgcBsqqLlg3G8EZpT9x-c4y-CxgCLcB/s1600/pouce%2Bbleu.png')
-    
+
             }
-    
+
         }
 
         tableauDeProduits.forEach(function (product) {
-    
+
             var emptyLike = aLike;
-    
-                emptyLike.addEventListener('click', function () {
+
+            emptyLike.addEventListener('click', function () {
                 product.like = !product.like;
                 console.log(product.like);
                 displayLike(emptyLike, product.like);
@@ -313,131 +313,153 @@ $.get ("http://localhost:10000", function (response, error){
 })*/
 
 //ONCLICK SUR MENU DANS LE HTML
- var menu = document.getElementById('menu');
- menu.addEventListener('click', function () {
- console.log('jai click');
- });
+var menu = document.getElementById('menu');
+menu.addEventListener('click', function () {
+    console.log('jai click');
+});
 
 //listener sur le voir dans le menu html
 var voir = document.getElementById("voir");
-voir.addEventListener("click",function(){
+voir.addEventListener("click", function () {
 
-//console.log("lalala");
-//DEMANDER LA DB PRODUCTS AU BACK TOUTE LA TABLE PRODUCTS (BACK>>APP GET SELECT PRODUCTS)
-$.get ("http://localhost:10000", function (response, error){ 
+    //console.log("lalala");
+    //DEMANDER LA DB PRODUCTS AU BACK TOUTE LA TABLE PRODUCTS (BACK>>APP GET SELECT PRODUCTS)
+    container2.innerHTML = "";
+    $.get("http://localhost:10000", function (response, error) {
         console.log(error);
-        console.log (response);
-  afficherPlusieursProduits(response);
-})
-console.log("good");
+        console.log(response);
+        afficherPlusieursProduits(response);
+    })
+    console.log("good");
 });
 
 //listener sur tshirts dans le menu html
 var tshirts = document.getElementById("tshirts");
-tshirts.addEventListener("click",function(){
-//APPEL LADRESSE TSHIRT VERS LE BACK (BACK>>APP GET /TSHIRT)
-//CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES TSHIRTS
-container.innerHTML="";
-$.get ("http://localhost:10000/tshirt",  function (response, error){
-afficherPlusieursProduits(response);
-})
+tshirts.addEventListener("click", function () {
+    //APPEL LADRESSE TSHIRT VERS LE BACK (BACK>>APP GET /TSHIRT)
+    //CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES TSHIRTS
+    container.innerHTML = "";
+    container2.innerHTML = "";
+    $.get("http://localhost:10000/tshirt", function (response, error) {
+        afficherPlusieursProduits(response);
+    })
 });
 
 //listener sur pantalons dans le menu html
 var pantalons = document.getElementById("pantalons");
-pantalons.addEventListener("click",function(){
-//APPEL LADRESSE PANTALON VERS LE BACK (BACK>>APP GET /PANTALON)
-//CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES PANTALONS
-    container.innerHTML="";
-$.get ("http://localhost:10000/pantalon",  function (response, error){
-afficherPlusieursProduits(response);
-})
-console.log("good");
+pantalons.addEventListener("click", function () {
+    //APPEL LADRESSE PANTALON VERS LE BACK (BACK>>APP GET /PANTALON)
+    //CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES PANTALONS
+    container.innerHTML = "";
+    container2.innerHTML = "";
+    $.get("http://localhost:10000/pantalon", function (response, error) {
+        afficherPlusieursProduits(response);
+    })
+    console.log("good");
 });
 
 //listener sur vestes dans le menu html
 var vestes = document.getElementById("vestes");
-vestes.addEventListener("click",function(){
- //APPEL LADRESSE VESTE VERS LE BACK (BACK>>APP GET /VESTE)
-//CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES VESTE
-container.innerHTML="";
-$.get ("http://localhost:10000/veste",  function (response, error){
-afficherPlusieursProduits(response);
-})
-console.log("good");
+vestes.addEventListener("click", function () {
+    //APPEL LADRESSE VESTE VERS LE BACK (BACK>>APP GET /VESTE)
+    //CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES VESTE
+    container.innerHTML = "";
+    container2.innerHTML = "";
+    $.get("http://localhost:10000/veste", function (response, error) {
+        afficherPlusieursProduits(response);
+    })
+    console.log("good");
 });
 
 //listener sur costumes dans le menu html
 var costumes = document.getElementById("costumes");
-costumes.addEventListener("click",function(){
-//APPEL LADRESSE COSTUME VERS LE BACK (BACK>>APP GET /COSTUME)
-//CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES COSTUME
-container.innerHTML="";
-$.get ("http://localhost:10000/costume",  function (response, error){
-afficherPlusieursProduits(response);
-})    
-console.log("good");
+costumes.addEventListener("click", function () {
+    //APPEL LADRESSE COSTUME VERS LE BACK (BACK>>APP GET /COSTUME)
+    //CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES COSTUME
+    container.innerHTML = "";
+    container2.innerHTML = "";
+    $.get("http://localhost:10000/costume", function (response, error) {
+        afficherPlusieursProduits(response);
+    })
+    console.log("good");
 });
 
 //listener sur robes dans le menu html
 var robes = document.getElementById("robes");
-robes.addEventListener("click",function(){
-//APPEL LADRESSE ROBE VERS LE BACK (BACK>>APP GET /ROBE)
-//CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES ROBE
-container.innerHTML="";
-$.get ("http://localhost:10000/robe",  function (response, error){
-afficherPlusieursProduits(response);
-})
-console.log("good");
+robes.addEventListener("click", function () {
+    //APPEL LADRESSE ROBE VERS LE BACK (BACK>>APP GET /ROBE)
+    //CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES ROBE
+    container.innerHTML = "";
+    container2.innerHTML = "";
+    $.get("http://localhost:10000/robe", function (response, error) {
+        afficherPlusieursProduits(response);
+    })
+    console.log("good");
 });
 
 //listener sur chaussure dans le menu html
 var chaussures = document.getElementById("chaussures");
-chaussures.addEventListener("click",function(){
+chaussures.addEventListener("click", function () {
     //APPEL LADRESSE CHAUSSURE VERS LE BACK (BACK>>APP GET /CHAUSSURE)
-//CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES CHAUSSURES
-container.innerHTML="";
-$.get ("http://localhost:10000/chaussure",  function (response, error){
-afficherPlusieursProduits(response);
-})
+    //CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES CHAUSSURES
+    container.innerHTML = "";
+    container2.innerHTML = "";
+    $.get("http://localhost:10000/chaussure", function (response, error) {
+        afficherPlusieursProduits(response);
+    })
 
-console.log("good");
+    console.log("good");
 });
 
 //listener sur accessoires dans le menu html
 var accessoires = document.getElementById("accessoires");
-accessoires.addEventListener("click",function(){
-//APPEL LADRESSE ACCESSOIRE VERS LE BACK (BACK>>APP GET /ACCESSOIRE)
-//CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES ACCESSOIRES
-container.innerHTML="";
-$.get ("http://localhost:10000/accessoire",  function (response, error){
-afficherPlusieursProduits(response);
-})
-console.log("good");
+accessoires.addEventListener("click", function () {
+    //APPEL LADRESSE ACCESSOIRE VERS LE BACK (BACK>>APP GET /ACCESSOIRE)
+    //CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES ACCESSOIRES
+    container.innerHTML = "";
+    container2.innerHTML = "";
+    $.get("http://localhost:10000/accessoire", function (response, error) {
+        afficherPlusieursProduits(response);
+    })
+    console.log("good");
 });
 
 //listener sur pyjamas dans le menu html
 var pyjamas = document.getElementById("pyjamas");
-pyjamas.addEventListener("click",function(){
-//APPEL LADRESSE PYJAMA VERS LE BACK (BACK>>APP GET /PYJAMA)
-//CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES PYAJAMAS
-container.innerHTML="";
-$.get ("http://localhost:10000/pyjama",  function (response, error){
-afficherPlusieursProduits(response);
-})
-console.log("good");
+pyjamas.addEventListener("click", function () {
+    //APPEL LADRESSE PYJAMA VERS LE BACK (BACK>>APP GET /PYJAMA)
+    //CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES PYAJAMAS
+    container.innerHTML = "";
+    container2.innerHTML = "";
+    $.get("http://localhost:10000/pyjama", function (response, error) {
+        afficherPlusieursProduits(response);
+    })
+    console.log("good");
 });
 
 //listener sur lingeries dans le menu html
 var lingeries = document.getElementById("lingeries");
-lingeries.addEventListener("click",function(){
-//APPEL LADRESSE LINGERIE VERS LE BACK (BACK>>APP GET /LINGERIE)
-//CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES LINGERIE
-container.innerHTML="";
-$.get ("http://localhost:10000/lingerie",  function (response, error){
-afficherPlusieursProduits(response);
-})
-console.log("good");
+lingeries.addEventListener("click", function () {
+    //APPEL LADRESSE LINGERIE VERS LE BACK (BACK>>APP GET /LINGERIE)
+    //CONTAINER VIDE POUR QUE AFFICHER UNIQUEMENT LES LINGERIE
+    container.innerHTML = "";
+    container2.innerHTML = "";
+    $.get("http://localhost:10000/lingerie", function (response, error) {
+        afficherPlusieursProduits(response);
+    })
+    console.log("good");
 });
 
 
+
+function send() {
+    // COTE FRONT
+
+    const input = document.getElementById('commentaire').value;
+    $.post(
+        'http://localhost:10000/comment', // defini l'URL d'envoie vers le serveur
+        { comment: input }, // defini ce qu'il envoie au serveur
+        function (response) { // defini la fonction execute quand le serveur repond
+            console.log(response);
+        });
+}
